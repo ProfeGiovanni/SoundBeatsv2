a66b395efd693d21d3946470c1f940f5b09e76c2
Giovanni García <giovannigarcia.de@hotmail.com>
2020-12-03 15:14:20 -0500

    Adición de controlador Song. (revisión entidades)

---
+++ SoundBeatsV2.Core/Domain/CountryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoundBeatsV2.Core.Domain
+{
+    public class CountryDTO
+    {
+        public int Id { get; set; }
+        public string CompoundName { get; set; }
+    }
+}
